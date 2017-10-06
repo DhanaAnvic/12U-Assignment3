cfbe68f72d921a440ba95787326762c7d712b493
fabed2976 <fabed2976@HRH-IC0027343.wrdsb.ca>
2017-10-06 09:39:22 -0400

    A3Q2 finished

--- src/A3Q2.java
+++ src/A3Q2.java
@@ -9,35 +9,57 @@
  */
 public class A3Q2 {
     
-    //create a method to return and array list and looking for the smalelst number in the list of array   
-    public int smallerMissingNumber(int []array, int start, int end){
-        
-    // create an if statement that is the starting number is greater than the ending number
-    // then it will return to the end and add 1
-    if (start > end){
-            return end + 1;
+    public void swap (int [] array, int pos1, int pos2){
+        //swapping two positons together
+        int temp = array[pos1];
+        array [pos1] = array [pos2];
+        array[pos2] = temp;
     }
-    // create an if statement that if the starting number is not equal to the first number in the array
-    // it will return to the starting point
-     if (start != array[start]){
-            return start;
-     }
-     //create a varible mid that adds the starying point to the ending number and divide it by 2
-     int mid = (start + end) / 2;
- 
-     // Left half has all elements from 0 to mid
-     if (array[mid] == mid){
-          return smallerMissingNumber(array, mid + 1, end);
+    
+        //create a method for the selection sort
+        public void selectionSort(int [] array){
+            //keeping track on what position we are in
+            for(int position = 0; position < array.length; position++ ){
+                //go through the rest of the numbers and find the smaller number
+                for(int i = position + 1; i < array.length; i++){
+                    //Did we find the smaller number
+                    if(array[i] < array[position]){
+                        //swap the numbers
+                        swap(array, i, position);
+                    }
+                }
+            }
         }
-        return smallerMissingNumber(array, start, mid);
+  
+    
+    //create a method to return and array list and looking for the smallest number in the list of array   
+    public int smallestMissingNum(int[]array){
+        // create a varible to start the position at 0
+            int missingNum = 0;
+            // if it does not work go back to selection sort
+            selectionSort(array);
+        
+            // create a for loop to go through the list of numbers
+        for(int j = 0; j < array.length; j++){
+            //if the missing number equals to the number it was looking for, it will ad 1 each time
+            if(missingNum == array[j]){
+                missingNum++;
+            }
+       
+        }
+        //return the missing number
+        return missingNum;
         
     }
 
     public static void main(String[] args) {
         // TODO code application logic here
         A3Q2 test = new A3Q2();
+        
+        //create an array of numbers
         int arr[] = {0, 1, 2, 3, 7, 8, 9, 11, 12};
-        int n = arr.length;
-        System.out.println("First Missing element is : " + test.smallerMissingNumber(arr, 0, n - 1));
+        
+        //print out the smallest numbers on the list of array listed above
+        System.out.println("the smallest missing number:" + test.smallestMissingNum(arr));
 }
 }
